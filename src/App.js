9e13f5b89326c9cbf234781693e9b803e1bffe47
Vivek <vivk84505@gmail.com>
2025-01-26 20:46:24 +0530

    app js changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< Updated upstream
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to React JS Vivek
-        </a>
-      </header>
-    </div>
-  );
-}
-=======
 import { useRoutes } from "react-router-dom";
 import Themeroutes from "./routes/Router";
 
@@ -31,6 +6,5 @@ const App = () => {
 
   return <div className="dark">{routing}</div>;
 };
->>>>>>> Stashed changes
 
 export default App;
